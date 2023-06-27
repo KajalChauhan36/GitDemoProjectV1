751dc5109b31a298aa9d0008af847cad57bc1048
KajalChauhan36 <131432927+KajalChauhan36@users.noreply.github.com>
2023-06-27 09:43:58 +0530

    First Commit

---
+++ src/Demo.java
@@ -0,0 +1,18 @@
+
+public class Demo {
+	public static void main(String[] args) {
+		
+	    String hungry="yes";
+	    if(hungry=="yes") 
+	    	System.out.println("Eat Something.............");
+	    	else{
+	    		System.out.println("Do Work.............");
+	    	
+	    }
+	    }  
+	   
+		 
+	} 
+		
+	 
+
