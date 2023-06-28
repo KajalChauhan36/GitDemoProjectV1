ec281c4313a9e29506d1509d5783d816ba8bc1e0
Mehak Oberoi <Mehak Oberoi@DESKTOP-V43PJ6C>
2023-06-28 15:28:19 +0530

    new change

--- src/Demo2.java
+++ src/Demo2.java
@@ -3,7 +3,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		
 	    String hungry="yes i am";
-	    if(hungry=="yes") 
+	    if(hungry=="yes/no") 
 	    	System.out.println("Eat Something.............");
 	    	else{
 	    		System.out.println("Do Work.............");
